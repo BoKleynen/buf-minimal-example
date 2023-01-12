dd63dc3159ab1545044a31f9d95c8b19c1315681
Bo Kleynen <kleynenbo@gmail.com>
2023-01-12 11:20:26 +0100

    make buf-generate

--- backend/pkg/pb/foo/baz/v1/baz_data.pb.go
+++ backend/pkg/pb/foo/baz/v1/baz_data.pb.go
@@ -184,9 +184,9 @@ type StabilityData struct {
 
 	// Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
 	// doloremque laudantium, totam rem aperiam, eaque ipsa :
-	// - quae ab illo inventore veritatis et quasi architecto beatae vitae.
-	// - dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-	//   aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui.
+	//   - quae ab illo inventore veritatis et quasi architecto beatae vitae.
+	//   - dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+	//     aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui.
 	Filters []string `protobuf:"bytes,1,rep,name=filters,proto3" json:"filters,omitempty"`
 }
 
